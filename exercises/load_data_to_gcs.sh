da1c9f0af542386d99eae3179e8bff440bbbc37d
behnam <bvakilir@partner.jaguarlandrover.com>
2022-10-25 08:46:38 +0000

    added gsutil flags

--- exercises/load_data_to_gcs.sh
+++ exercises/load_data_to_gcs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
 
+# TODO: change it to your name
+DESTINATION_BUCKET_NAME=2022-jlr-temp-<your-name>
 
-DESTINATION_BUCKET_NAME=<my-bucket-name>
-
-gsutil mb gs://$DESTINATION_BUCKET_NAME
+gsutil mb -c standard -l EU gs://$DESTINATION_BUCKET_NAME
 
 gsutil cp -r ./dataset/* gs://$DESTINATION_BUCKET_NAME/dataset/
